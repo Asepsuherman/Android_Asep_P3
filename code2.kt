63842feda9ea693a03085192e2fcd26172cf80a6
Asep Suherman <74848442+Asepsuherman@users.noreply.github.com>
2023-09-22 11:29:43 +0700

    Update code2.kt
    Tambah keterangan codelab2

--- code2.kt
+++ code2.kt
@@ -1,11 +1,14 @@
+// *** Membuat dan Menggunakan fungsi di kotlin
 fun main() {
-	// Membuat variabel dengan value number.
+    // Membuat variabel dengan value number.
     var count = 22
-    // Ini akan mencetak output sesuai pesan didalamnya,
-    // dan memanggil variabel count yang mempunyai value 22
+    // Println ini akan mencetak output standar sesuai pesan didalamnya dan memanggil variabel $count yang mempunyai value 22
     println("Umur asep $count")
+    // output: Umur asep 22
 
     // Ini akan mengurangi 1 nilai dari value variabel count yang ada menjadi 21
     count--
     println("Sebelumnya umur Asep $count")
+    // output: Sebelumnya umur asep 21
+
 }
